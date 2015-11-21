80e70311b16c51d69493c182bc2d7af713bcc8c8
Junming Chen <CJM@Junmings-MacBook-Pro.local>
2015-11-20 23:26:13 -0800

    data -v2

--- cleanResultgoodreads.py
+++ cleanResultgoodreads.py
@@ -13,3 +13,19 @@ for line in f1.readlines():
 f2.write("]")
 f1.close()
 f2.close()
+
+f1 = open("goodreadsauthor.json", "r")
+f2 = open("goodreadsauthorclean.json", "w")
+
+f2.write("[")
+for line in f1.readlines():
+	if line[0]=='}':
+		f2.write(line)
+		f2.write(",\n")
+	else:
+		f2.write(line)
+
+
+f2.write("]")
+f1.close()
+f2.close()
